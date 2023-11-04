de99d9a66114e2c2b944d43fcc6ce4e4bab38389
Arafat Azam <arafatazam17@gmail.com>
2023-11-04 02:44:26 +0000

    Demo modified

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 from collections import deque, defaultdict
 
 
-alarming_actions = ['hit', 'jump', 'punch', 'run', 'shoot_gun','throw']
+alarming_actions = ['hit', 'jump', 'punch', 'run', 'shoot_gun', 'throw']
 normal_actions = ['walk']
 processed_path = "hmdb51_processed"
 actions = np.array(
@@ -15,11 +15,13 @@ label_map = {label: idx for idx, label in enumerate(actions)}
 
 frames_map = defaultdict(lambda: deque(maxlen=16))
 
+
 def save_frame(id, frame):
     if len(frames_map[id]) == 0:
         for i in range(16):
             frames_map[id].append(np.zeros_like(frame))
     frames_map[id].append(frame)
+
 
 def get_frames(id):
 
@@ -27,13 +29,10 @@ def get_frames(id):
 
     return np.array([frames_map[id]])
 
+
 yolo = YOLO('yolov8n-pose.pt')
 model = tf.keras.models.load_model('model.h5')
-cap = cv2.VideoCapture("https://www.youtube.com/watch?v=9-gWNtrH1e0")
-
-actual_fps = cap.get(cv2.CAP_PROP_FPS)
-desired_fps = 10
-skip_frame = actual_fps // desired_fps
+cap = cv2.VideoCapture(0)
 
 frame_counter = 0
 label = defaultdict(lambda: 'No Label')
@@ -42,23 +41,27 @@ while True:
     if not ret:
         break
     frame_counter += 1
+
     results = yolo.track(frame, persist=True)
     boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
-    if len(boxes)>0:
+    if len(boxes) > 0:
         ids = results[0].boxes.id.cpu().numpy().astype(int)
     else:
         ids = []
     print('ids', ids)
     i = 0
     for box, id in zip(boxes, ids):
-        if frame_counter == skip_frame:
+        if frame_counter % 2 == 0:
             keypoints = results[0].keypoints[i].xyn[0].numpy().flatten()
             save_frame(id, keypoints)
             frames = get_frames(id)
             prediction = model.predict(frames)
-            print('Prediction ', prediction)
+
             label_idx = np.argmax(prediction)
-            label[id] = actions[label_idx]
+            if prediction[0][label_idx] > 0.3:
+                label[id] = actions[label_idx]
+            else:
+                label[id] = 'Person'
 
         if label[id] in alarming_actions:
             color = (0, 0, 255)
@@ -67,7 +70,7 @@ while True:
 
         cv2.rectangle(frame, (box[0], box[1]),
                       (box[2], box[3]), color, 2)
-        
+
         cv2.putText(
             frame,
             f"Id {id} Lbl-{label[id]}",
@@ -77,9 +80,8 @@ while True:
             color,
             2,
         )
-        i+=1
+        i += 1
 
-    frame_counter %= skip_frame
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
