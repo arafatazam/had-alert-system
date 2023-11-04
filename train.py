dc1949023beb3c698b615c73c228e1a244fbaf38
Arafat Azam <arafatazam17@gmail.com>
2023-11-04 02:44:13 +0000

    Early stopping implemented and matrices shown

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
 from keras.utils import to_categorical
-from keras.callbacks import TensorBoard
+from keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 
 
@@ -27,7 +28,7 @@ def create_model():
     model.add(Dense(32, activation='relu'))
     model.add(Dense(actions.shape[0], activation='softmax'))
     model.compile(optimizer='Adam', loss='categorical_crossentropy',
-                  metrics=['categorical_accuracy'])
+                  metrics=['accuracy'])
     return model
 
 
@@ -54,16 +55,34 @@ def run():
     model = create_model()
     X, y = get_data_and_labels()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
-    log_dir = os.path.join('log')
-    callback = TensorBoard(log_dir=log_dir)
-    model.fit(X_train, y_train, epochs=200 , callbacks=[callback])
+    early_stopping_callback = EarlyStopping(
+        monitor='val_loss', patience=10, mode='min', restore_best_weights=True)
+
+    history = model.fit(X_train, y_train, epochs=200, validation_data=(X_test, y_test), callbacks=[early_stopping_callback])
     model.summary()
 
     res = model.evaluate(X_test, y_test)
     print('Test Accuracy:', res)
 
-    model.save('model.h5')
+    # Plot the training and validation accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Task 3: Model accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(['Train', 'Validation'])
+    plt.show()
 
+    # Plot the training and validation loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Task 3: Model loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['Train', 'Validation'])
+    plt.show()
+
+    model.save('model.h5')
 
 
 if __name__ == '__main__':
